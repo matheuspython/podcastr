51daeb6b7f629fb7949301faa3e9ddbb9af8cebe
matheus souza <ruivorobado@gmail.com>
2021-04-22 17:23:28 -0300

    exemplo de propriedade e componentes

---
+++ podcastr/src/Button.js
@@ -0,0 +1,6 @@
+export default function Button(props){
+ 
+  return (
+    <button>{props.title}</button>
+  )
+}
--- podcastr/src/index.js
+++ podcastr/src/index.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import Button from './Button';
 
 
 ReactDOM.render(
-    <App />,
+    <>
+      <Button title="button 1"/>
+      <Button title="button 2"/>
+      <Button title="button 3"/>
+      <Button title="button 4"/>
+      <Button title="button 5"/>
+    </>
+    ,
   document.getElementById('root')
 );
